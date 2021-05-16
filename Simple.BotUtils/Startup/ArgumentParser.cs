9866c616cb49b9338dc7bf734a55957ea1f9225e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-16 11:57:17 -0300

    Add argument parser module

---
+++ Simple.BotUtils/Startup/ArgumentParser.cs
@@ -0,0 +1,28 @@
+﻿namespace Simple.BotUtils.Startup
+{
+    public class ArgumentParser
+    {
+        public static Arguments Parse(string[] args)
+        {
+           var collection = new Arguments();
+
+            string last = "";
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].StartsWith("-"))
+                {
+                    string key = args[i];
+                    last = key;
+                    collection.Add(key, "");
+                }
+                else
+                {
+                    collection[last] = args[i];
+                    last = "";
+                }
+            }
+
+            return collection;
+        }
+    }
+}
---
+++ Simple.BotUtils/Startup/Arguments.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Simple.BotUtils.Startup
+{
+    public class Arguments : Dictionary<string, string>
+    {
+        public bool Has(string key) => ContainsKey(key);
+
+        public string Get(string key)
+        {
+            if (TryGetValue(key, out string val)) return val;
+            return null;
+        }
+    }
+}
