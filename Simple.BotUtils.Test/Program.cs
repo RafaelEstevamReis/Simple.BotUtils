13f07c458eef4b59b940811b7b13405880684ec6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-22 18:41:53 -0300

    Add test project

---
+++ Simple.BotUtils.Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using Simple.BotUtils.Caching;
+using System;
+
+namespace Simple.BotUtils.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cache = new MemoryCache();
+            cache.Add("USERS", new CacheOptions()
+            {
+                ExpirationPolicy = ExpirationPolicy.LastAccess,
+                ExpirationValue = TimeSpan.FromHours(1),
+                UpdateCallback = () => new long[] { 12345566 },
+            });
+
+            var data = cache.Get<long[]>("USERS");
+            data = data;
+        }
+    }
+}
