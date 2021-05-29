790c52925a751c744be4a0c0a43f2d6558b4e504
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-29 15:28:37 -0300

    Create ConfigBase.cs

---
+++ Simple.BotUtils/Data/ConfigBase.cs
@@ -0,0 +1,23 @@
+﻿namespace Simple.BotUtils.Data
+{
+    public abstract class ConfigBase
+    {
+        public void Save(string filePath) => XmlSerializer.ToFile(filePath, this);
+
+        public static T Load<T>(string FilePath)
+            where T : ConfigBase
+        {
+            return XmlSerializer.FromFile<T>(FilePath);
+        }
+        public static T LoadOrCreate<T>(string FilePath)
+            where T : ConfigBase, new()
+        {
+            return XmlSerializer.LoadOrCreate<T>(FilePath, new T());
+        }
+        public static T LoadOrCreate<T>(string FilePath, T template) 
+            where T : ConfigBase
+        {
+            return XmlSerializer.LoadOrCreate<T>(FilePath, template);
+        }
+    }
+}
