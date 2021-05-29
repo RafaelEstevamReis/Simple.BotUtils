3445755b6b00e5b8e342f3709ffdce6a438b52f7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-29 13:24:02 -0300

    Add SimpleJob

---
+++ Simple.BotUtils/Jobs/Job.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Simple.BotUtils.Jobs
+{
+    public class Job : IJob
+    {
+        protected Job() { }
+        public Job(Action<ExecutionTrigger, object> runAction,
+                   bool runOnStartUp = false,
+                   TimeSpan? startEvery = null,
+                   bool canBeInvoked = false)
+        {
+            RunAction = runAction ?? throw new ArgumentNullException(nameof(runAction));
+
+            CanBeInvoked = canBeInvoked;
+            RunOnStartUp = runOnStartUp;
+            if (startEvery.HasValue)
+            {
+                CanBeScheduled = true;
+                StartEvery = startEvery.Value;
+            }
+            else
+            {
+                CanBeScheduled = false;
+            }
+        }
+
+        public bool CanBeInvoked { get; set; }
+        public bool CanBeScheduled { get; set; }
+        public bool RunOnStartUp { get; set; }
+        public TimeSpan StartEvery { get; set; }
+
+        public Action<ExecutionTrigger, object> RunAction { get; }
+
+#if NET40
+        public virtual Task ExecuteAsync(ExecutionTrigger trigger, object parameter)
+        {
+            return Task.Factory.StartNew(() => RunAction(trigger, parameter));
+        }
+#else
+        public virtual async Task ExecuteAsync(ExecutionTrigger trigger, object parameter)
+        {
+            await Task.Run(() => RunAction(trigger, parameter));
+        }
+#endif
+    }
+}
