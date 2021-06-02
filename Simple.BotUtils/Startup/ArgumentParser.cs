2d86b58cbf212024d0cf06ab19441fa5f0d706f5
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-06-02 18:44:33 -0300

    New ArgumentParser into Class

--- Simple.BotUtils/Startup/ArgumentParser.cs
+++ Simple.BotUtils/Startup/ArgumentParser.cs
@@ -1,10 +1,13 @@
-﻿namespace Simple.BotUtils.Startup
+﻿using System;
+using System.Linq;
+
+namespace Simple.BotUtils.Startup
 {
     public class ArgumentParser
     {
         public static Arguments Parse(string[] args)
         {
-           var collection = new Arguments();
+            var collection = new Arguments();
 
             string last = "";
             for (int i = 0; i < args.Length; i++)
@@ -24,5 +27,56 @@
 
             return collection;
         }
+
+#if !NETSTANDARD1_0
+
+        public static T ParseAs<T>(string[] args)
+            where T : new() => ParseInto<T>(args, new T());
+
+        public static T ParseInto<T>(string[] args, T template)
+            where T : new() => MapTo<T>(Parse(args), template);
+
+        public static T MapTo<T>(Arguments arguments, T obj)
+            where T : new()
+        {
+            if(obj == null) obj = new T();
+
+            var type = typeof(T);
+            foreach (var prop in type.GetProperties())
+            {
+                if (!prop.CanRead) continue;
+                if (!prop.CanWrite) continue;
+                // same name
+                var nameOptions = new string[] { $"--{prop.Name}" };
+                
+                // ArgumentKey Attributes
+                var attrArgKeys = prop.GetCustomAttributes(false)
+                                     .OfType<ArgumentKeyAttribute>();
+                // Search
+                var allKeys = attrArgKeys.SelectMany(o => o.Keys)
+                                         .Union(nameOptions);
+                foreach (var k in allKeys)
+                {
+                    if (arguments.Has(k))
+                    {
+                        setPropValue(prop, arguments[k], obj);
+                        break;
+                    }
+                }
+            }
+            return obj;
+        }
+        private static void setPropValue<T>(System.Reflection.PropertyInfo prop, string value, T obj)
+        {
+            object oValue = value;
+            // Not string? try to convert
+            if (prop.PropertyType != typeof(string))
+            {
+                oValue = Convert.ChangeType(oValue, prop.PropertyType);
+            }
+            prop.SetValue(obj, oValue, index: null);
+        }
+#endif
+
     }
 }
---
+++ Simple.BotUtils/Startup/Attributes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Simple.BotUtils.Startup
+{
+    public class ArgumentKeyAttribute : Attribute
+    {
+        public string[] Keys { get; }
+        public ArgumentKeyAttribute(params string[] keys)
+        {
+            Keys = keys;
+        }
+    }
+}
