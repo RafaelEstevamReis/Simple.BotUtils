cba8c78d5d3b9ff8846b446a4fff418f208493f1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-06-02 18:44:44 -0300

    Add sample

--- Simple.BotUtils.Test/Program.cs
+++ Simple.BotUtils.Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Simple.BotUtils.Caching;
+using Simple.BotUtils.Data;
 using System;
 
 namespace Simple.BotUtils.Test
@@ -8,17 +9,36 @@ namespace Simple.BotUtils.Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Create fake args
+            args = new string[] { "-a", "nothing", "-n", "da-bot", "--MyLongNumber", "684261" };
 
-            var cache = new MemoryCache();
-            cache.Add("USERS", new CacheOptions()
-            {
-                ExpirationPolicy = ExpirationPolicy.LastAccess,
-                ExpirationValue = TimeSpan.FromHours(1),
-                UpdateCallback = () => new long[] { 12345566 },
-            });
+            // load my config from Disk
+            var cfg = ConfigBase.Load<MyConfig>("cfg.xml");
 
-            var data = cache.Get<long[]>("USERS");
-            data = data;
+            // Apply args
+            Startup.ArgumentParser.ParseInto(args, cfg);
+            cfg.Save<MyConfig>();
+
+            // continue ...
+            Console.WriteLine(cfg);
         }
     }
+
+    public class MyConfig : ConfigBase
+    {
+        [Startup.ArgumentKey("-n", "--name")]
+        public string MyName { get; set; }
+        [Startup.ArgumentKey("-i", "--info")]
+        public string MyInfo { get; set; }
+        [Startup.ArgumentKey("-nb", "--number")]
+        public int MyNumber { get; set; }
+        [Startup.ArgumentKey("-ln", "--long")]
+        public long MyLongNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"{MyName} | {MyInfo} | {MyNumber}: {MyLongNumber}";
+        }
+    }
+
 }
