e0702f24dc7ba06dba0d86c7bbe35470a4a764d1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-07-25 12:03:00 -0300

    Fix NETSTD and NETFx StartsWith and EndsWith

---
+++ Simple.BotUtils/Compatiblity/NET4X_String.cs
@@ -0,0 +1,28 @@
+﻿#if NET40_OR_GREATER || NETSTANDARD
+namespace Simple.BotUtils
+{
+    internal static class NET4X_String
+    {
+        public static bool EndsWith(this string text, char value)
+        {
+            int thisLen = text.Length;
+            if (thisLen != 0)
+            {
+                if (text[thisLen - 1] == value)
+                    return true;
+            }
+            return false;
+        }
+        public static bool StartsWith(this string text, char value)
+        {
+            int thisLen = text.Length;
+            if (thisLen != 0)
+            {
+                if (text[0] == value)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+#endif
