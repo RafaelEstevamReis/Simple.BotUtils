24dc9a1e9b09d0b2fd370f69d9422052d3cf94fc
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-08-10 11:56:08 -0300

    Remove mediator

--- Simple.BotUtils/Messages/IMessenger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Simple.BotUtils.Messages
-{
-    public interface IMessengerBase { }
-    public interface IMessenger<Req, Resp>
-        : IMessengerBase
-    {
-        Resp Send(Req request);
-    }
-}
