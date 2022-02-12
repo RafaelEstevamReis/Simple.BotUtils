6253550859c07290a201d30c235d8b889541412c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-12 19:50:14 -0300

    Create Exceptions.cs

---
+++ Simple.BotUtils/Controllers/Exceptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Simple.BotUtils.Controllers
+{
+    public class InvalidMethodParameterTypeException
+        : Exception
+    {
+        public string ParameterName { get; private set; }
+        public Type ParameterType { get; private set; }
+        public Type GivenType { get; private set; }
+
+        public InvalidMethodParameterTypeException(string message, string parameterName, Type parameterType, Type givenType)
+            : base(message)
+        {
+            ParameterName = parameterName;
+            ParameterType = parameterType;
+            GivenType = givenType;
+        }
+    }
+    public class NoSuitableMethodFound
+            : Exception
+    {
+        public string MethodName { get; private set; }
+
+        public NoSuitableMethodFound(string methodName)
+            : base($"No suitable Method {methodName} found with matching parameter count. Special parameters such as [FromDI] are not counted.")
+        {
+            MethodName = methodName;
+        }
+    }
+}
