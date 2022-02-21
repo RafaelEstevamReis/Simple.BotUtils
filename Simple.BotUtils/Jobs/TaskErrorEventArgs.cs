30068eaa2f5ece043aa492d97d8939c49eb71b69
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-21 01:05:02 -0300

    Create TaskErrorEventArgs.cs

---
+++ Simple.BotUtils/Jobs/TaskErrorEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Simple.BotUtils.Jobs
+{
+    public class TaskErrorEventArgs : EventArgs
+    {
+        public JobInfo Info { get; }
+        public Exception Exception { get; }
+
+        public TaskErrorEventArgs(JobInfo info, Exception ex)
+        {
+            Info = info;
+            Exception = ex;
+        }
+
+    }
+}
