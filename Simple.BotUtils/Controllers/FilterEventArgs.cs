235e56d4f1f5005be95164a547590383359b9a9c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-26 02:04:34 -0300

    Create FilterEventArgs.cs

---
+++ Simple.BotUtils/Controllers/FilterEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple.BotUtils.Controllers
+{
+    public class FilterEventArgs : EventArgs
+    {
+        public string Method { get; set; }
+        public object[] Args { get; set; }
+        public Attribute[] Attrbiutes { get;  set; }
+        public FilterException BlockReason { get; set; }
+    }
+}
