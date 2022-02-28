54d94c6bd8aee92e1d44f9b6f37d65d53de48852
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-28 02:14:40 -0300

    Create InteractiveMenuExample.cs

---
+++ Simple.BotUtils.Test/TerminalSamples/InteractiveMenuExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Simple.BotUtils.Test.TerminalSamples
+{
+    public class InteractiveMenuExample
+    {
+        public static void ProgramMain(string[] args)
+        {
+            var iMnu = new Terminal
+                .InteractiveMenu()
+                .SetCaption("Test caption")
+                .SetSelectedBgColor(ConsoleColor.DarkGreen)
+                .SetSelectedFgColor(ConsoleColor.White)
+                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 1" })
+                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 2", HotKey = ConsoleKey.F4 })
+                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 3" })
+                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 4", HotKey = ConsoleKey.Escape })
+                ;
+            var opt2 = iMnu.ShowMenu();
+            Console.WriteLine(opt2);
+
+        }
+    }
+}
