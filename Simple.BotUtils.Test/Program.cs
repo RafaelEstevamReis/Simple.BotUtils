5c26f30d75868301d4c8a904a556957b18be617d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-28 02:16:37 -0300

    Update Program.cs

--- Simple.BotUtils.Test/Program.cs
+++ Simple.BotUtils.Test/Program.cs
@@ -1,7 +1,27 @@
-﻿using System;
+﻿var menu = new Simple.BotUtils.Terminal
+    .SimpleMenu()
+    .SetCaption("Choose the sample")
+    .AddOption("Configuration Example")
+    .AddOption("Controller Example")
+    .AddOption("Schedule Example")
+    .AddOption("Interactive Menu Example")
+    ;
 
-Console.WriteLine("Choose the sample");
+var opt = menu.ShowMenu(true);
+switch (opt)
+{
+    case 0:
+        ConfigurationSample.FullExample.ProgramMain(args);
+        break;
+    case 1:
+        ControllerSample.FullExample.ProgramMain(args);
+        break;
+    case 2:
+        ScheduleSample.FullExample.ProgramMain(args);
+        break;
 
-ConfigurationSample.FullExample.ProgramMain(args);
-ControllerSample.FullExample.ProgramMain(args);
-ScheduleSample.FullExample.ProgramMain(args);
+    case 3:
+        TerminalSamples.InteractiveMenuExample.ProgramMain(args);
+        break;
+}
+
