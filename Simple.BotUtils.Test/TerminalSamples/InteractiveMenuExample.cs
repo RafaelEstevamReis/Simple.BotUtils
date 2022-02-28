7461f1a6aeb9341489693926dc143555f33c9bc7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-28 02:16:32 -0300

    Update InteractiveMenuExample.cs

--- Simple.BotUtils.Test/TerminalSamples/InteractiveMenuExample.cs
+++ Simple.BotUtils.Test/TerminalSamples/InteractiveMenuExample.cs
@@ -1,20 +1,20 @@
-﻿using System;
+﻿using Simple.BotUtils.Terminal;
+using System;
 
-namespace Simple.BotUtils.Test.TerminalSamples
+namespace TerminalSamples
 {
     public class InteractiveMenuExample
     {
         public static void ProgramMain(string[] args)
         {
-            var iMnu = new Terminal
-                .InteractiveMenu()
+            var iMnu = new InteractiveMenu()
                 .SetCaption("Test caption")
                 .SetSelectedBgColor(ConsoleColor.DarkGreen)
                 .SetSelectedFgColor(ConsoleColor.White)
-                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 1" })
-                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 2", HotKey = ConsoleKey.F4 })
-                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 3" })
-                .AddOption(new Terminal.InteractiveMenu.MenuOption() { Text = "Option 4", HotKey = ConsoleKey.Escape })
+                .AddOption(new InteractiveMenu.MenuOption() { Text = "Option 1" })
+                .AddOption(new InteractiveMenu.MenuOption() { Text = "Option 2", HotKey = ConsoleKey.F4 })
+                .AddOption(new InteractiveMenu.MenuOption() { Text = "Option 3" })
+                .AddOption(new InteractiveMenu.MenuOption() { Text = "Option 4", HotKey = ConsoleKey.Escape })
                 ;
             var opt2 = iMnu.ShowMenu();
             Console.WriteLine(opt2);
