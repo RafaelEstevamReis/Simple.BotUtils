8a101f97d49bbf1db1d0ac9619204614f3a2d2b2
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-31 09:27:41 -0300

    Add ctor params

--- Simple.BotUtils/Helpers/TypeHelper.cs
+++ Simple.BotUtils/Helpers/TypeHelper.cs
@@ -9,23 +9,36 @@ namespace Simple.BotUtils.Helpers
 {
     public static class TypeHelper
     {
-        public static IEnumerable<Type> GetClassesOfType<T>(Assembly source, Func<Type,bool> filter = null)
+        /// <summary>
+        /// Returns all class-types assinable from T in the assembly
+        /// </summary>
+        public static IEnumerable<Type> GetClassesOfType<T>(Assembly source, Func<Type, bool> filter = null)
         {
             var interfaceType = typeof(T);
             var types = source.GetTypes()
                               .Where(t => t.IsClass)
                               .Where(t => interfaceType.IsAssignableFrom(t));
 
-            if(filter is not null)
+            if (filter is not null)
             {
                 types = types.Where(filter);
             }
 
             return types;
         }
+        /// <summary>
+        /// Create T instances from provided types
+        /// </summary>
         public static IEnumerable<T> CreateInstancesFor<T>(IEnumerable<Type> types)
         {
-            return types.Select(t => Activator.CreateInstance<T>());
+            return types.Select(t => (T)Activator.CreateInstance(t));
+        }
+        /// <summary>
+        /// Create T instances from provided types
+        /// </summary>
+        public static IEnumerable<T> CreateInstancesFor<T>(IEnumerable<Type> types, params object[] args)
+        {
+            return types.Select(t => (T)Activator.CreateInstance(t, args));
         }
     }
 }
