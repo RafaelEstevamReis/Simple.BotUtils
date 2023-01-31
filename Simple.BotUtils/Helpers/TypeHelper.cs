d72601c9d6986134ceedbf75916bd7842cb33d88
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-31 08:15:09 -0300

    Add TypeHelper

---
+++ Simple.BotUtils/Helpers/TypeHelper.cs
@@ -0,0 +1,32 @@
+﻿#if !NETSTANDARD1_0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Simple.BotUtils.Helpers
+{
+    public static class TypeHelper
+    {
+        public static IEnumerable<Type> GetClassesOfType<T>(Assembly source, Func<Type,bool> filter = null)
+        {
+            var interfaceType = typeof(T);
+            var types = source.GetTypes()
+                              .Where(t => t.IsClass)
+                              .Where(t => interfaceType.IsAssignableFrom(t));
+
+            if(filter is not null)
+            {
+                types = types.Where(filter);
+            }
+
+            return types;
+        }
+        public static IEnumerable<T> CreateInstancesFor<T>(IEnumerable<Type> types)
+        {
+            return types.Select(t => Activator.CreateInstance<T>());
+        }
+    }
+}
+#endif
