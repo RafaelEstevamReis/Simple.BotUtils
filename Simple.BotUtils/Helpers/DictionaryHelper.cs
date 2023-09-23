92b4c12d8d69128133bf91a2dca13eab598f9138
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-09-23 12:44:06 -0300

    Create DictionaryHelper.cs

---
+++ Simple.BotUtils/Helpers/DictionaryHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Simple.BotUtils.Helpers;
+
+using System.Collections.Generic;
+
+public static class DictionaryHelper
+{
+    public static T Get<K, T>(this Dictionary<K, T> dic, K key, T def)
+    {
+        if (dic.ContainsKey(key)) return dic[key];
+        return def;
+    }
+
+}
