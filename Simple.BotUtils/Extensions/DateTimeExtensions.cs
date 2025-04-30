b2c035b83a53b711c72adbdf6fff4b039476711c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-04-30 18:53:31 -0300

    Create DateTimeExtensions.cs

---
+++ Simple.BotUtils/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Simple.BotUtils;
+
+using System;
+
+public static class DateTimeExtensions
+{
+    public static TimeSpan? Age(this DateTime? dateTime) => dateTime == null ? null : (DateTime.UtcNow - dateTime);
+    public static TimeSpan Age(this DateTime dateTime) => DateTime.UtcNow - dateTime;
+
+    public static DateTime FromUnixSeconds(this long seconds) => GetUnixEpoch().AddSeconds(seconds);
+    public static DateTime FromUnixMilis(this long milis) => GetUnixEpoch().AddMilliseconds(milis);
+
+
+#if NETSTANDARD || NET20_OR_GREATER
+    private static readonly DateTime Epoch = new DateTime(1970, 1, 1);
+    public static DateTime GetUnixEpoch() => Epoch;
+#else
+    public static DateTime GetUnixEpoch() => DateTime.UnixEpoch;
+#endif
+}
