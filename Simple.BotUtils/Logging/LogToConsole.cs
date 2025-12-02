a7f2eafaed74cbce9209d00fee769b5d2e6258a1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-12-02 16:40:16 -0300

    [LogToConsole] Add minLevel

--- Simple.BotUtils/Logging/LogToConsole.cs
+++ Simple.BotUtils/Logging/LogToConsole.cs
@@ -5,23 +5,31 @@ using System;
 
 public class LogToConsole : ILogger
 {
+    public LogEventLevel MinLevel { get; set; }
+
+    public LogToConsole(LogEventLevel minLevel = LogEventLevel.Information)
+    {
+        MinLevel = minLevel;
+    }
+
     public void Information(string messageTemplate, params object[] propertyValues)
     {
-        Console.WriteLine(Logger.MessageBuider(LogEventLevel.Information, true, messageTemplate, propertyValues));
+        write(LogEventLevel.Information, messageTemplate, propertyValues);
     }
 
     public void Warning(string messageTemplate, params object[] propertyValues)
     {
-        Console.WriteLine(Logger.MessageBuider(LogEventLevel.Warning, true, messageTemplate, propertyValues));
+        write(LogEventLevel.Warning, messageTemplate, propertyValues);
     }
 
     public void Error(string messageTemplate, params object[] propertyValues)
     {
-        Console.WriteLine(Logger.MessageBuider(LogEventLevel.Error, true, messageTemplate, propertyValues));
+        write(LogEventLevel.Error, messageTemplate, propertyValues);
     }
 
     public void Error(Exception exception, string messageTemplate, params object[] propertyValues)
     {
+        if (MinLevel > LogEventLevel.Error) return;
         Console.WriteLine(Logger.MessageBuider(LogEventLevel.Error, true, messageTemplate, propertyValues));
         Console.WriteLine($"Exception: {exception.Message}");
         Console.WriteLine($"StackTrace: {exception.StackTrace}");
@@ -29,12 +37,18 @@ public class LogToConsole : ILogger
 
     public void Fatal(string messageTemplate, params object[] propertyValues)
     {
-        Console.WriteLine(Logger.MessageBuider(LogEventLevel.Fatal, true, messageTemplate, propertyValues));
+        write(LogEventLevel.Fatal, messageTemplate, propertyValues);
     }
 
     public void Debug(string messageTemplate, params object[] propertyValues)
     {
-        Console.WriteLine(Logger.MessageBuider(LogEventLevel.Debug, true, messageTemplate, propertyValues));
+        write(LogEventLevel.Debug, messageTemplate, propertyValues);
+    }
+
+    private void write(LogEventLevel level, string messageTemplate, params object[] propertyValues)
+    {
+        if (MinLevel > level) return;
+        Console.WriteLine(Logger.MessageBuider(level, true, messageTemplate, propertyValues));
     }
 
 }
